3cecb6714c5ea8ea0462f5af48426ded262497b0
antav7s <86698388+antav7s@users.noreply.github.com>
2022-04-23 19:12:09 -0500

    MySQL_CONNECT
    MySQL_CONNECT

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-const express = require('express')
-const morgan = require('morgan')
 const mysql = require('mysql2')
 
-const app = express()
-
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
-
-// https://gist.githubusercontent.com/meech-ward/1723b2df87eae8bb6382828fba649d64/raw/ee52637cc953df669d95bb4ab68ac2ad1a96cd9f/lotr.sql
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -14,48 +7,23 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE,
 })
 
-function getRandomInt(max) {
-  return 1 + Math.floor(Math.random() * (max-1))
-}
-
-async function getCharacter(id) {
-  const [characters] = await pool.promise().query("SELECT * FROM characters WHERE id = ?", [
-    id,
-  ])
-  return characters[0]
-}
-async function randomId() {
-  const [rows] = await pool.promise().query(
-    "SELECT COUNT(*) as totalCharacters FROM characters"
-  )
-  const { totalCharacters } = rows[0]
-  const randomId = getRandomInt(totalCharacters)
-  return randomId
-}
-
 app.get("/test", (req, res) => {
-  res.send("<h1>It's working 🤗</h1>")
+  res.send("<h1>OK</h1>")
 })
 
-app.get("/", async (req, res) => {
-  try {
-    const id = await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
-  } catch (error) {
-    res.send(error)
-  }
+app.get("/:name", async (req, res) => {
+        const query = "SELECT name, owner, species FROM pet WHERE name = ?";
+        pool.query(query, [ req.params.name], (error, results) => {
+            if (!results[0]){
+            res.json({ status: "Not Found!" });
+
+            } else {
+                        res.json(results[0]);
+            }
+             }
+)
 })
 
-app.get("/:id", async (req, res) => {
-  try {
-    const id = parseInt(req.params.id) || await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
-  } catch (error) {
-    res.send(error)
-  }
-})
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Listening on port ${port}`))
